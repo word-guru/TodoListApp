2fc1c56cbddcf4045d70b334e661f02ec3bf6cdd
Pavel Popov <legeon48@mail.ru>
2022-05-15 18:49:02 +0300

    Update TodoModel

--- TodoList/Models/TodoModel.cs
+++ TodoList/Models/TodoModel.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace TodoList.Models
 {
-    internal class TodoModel
+    public class TodoModel
     {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        private bool _isDone;
+        private string _text;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+
     }
 }
