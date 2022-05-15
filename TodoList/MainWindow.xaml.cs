05b48c1c04426b470874e0964cbdbef8674cbe01
Pavel Popov <legeon48@mail.ru>
2022-05-15 18:57:57 +0300

    Create Loaded

--- TodoList/MainWindow.xaml.cs
+++ TodoList/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TodoList.Models;
 
 namespace TodoList
 {
@@ -20,9 +22,19 @@ namespace TodoList
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<TodoModel> _todoData;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoData = new BindingList<TodoModel>()
+            {
+                new TodoModel(){Text = "text"},
+                new TodoModel(){Text = "test"}
+            };
         }
     }
 }
